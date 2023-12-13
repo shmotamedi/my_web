4a2860abcd27955189b8beeb139ec02247d32e8c
shahryar motamedi <shahryar.motamedi@yahoo.com>
2023-12-13 16:05:34 +0330

    wellcome part was completed

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def wellcome(request):
-    return HttpResponse("<h1>Wellcome to my web<h1>")
+    text1="<h1>Wellcome to my web<h1>"
+    text2="<h2>Wellcome to my web<h2>"
+    text3="<h3>Wellcome to my web<h3>"
+    note=[text3,text2,text1]
+    note="\n".join(note)
+    return HttpResponse(note)
