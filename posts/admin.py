0e806e320bb12a7700600a285fccaf4ddb9d679f
shahryar motamedi <shahryar.motamedi@yahoo.com>
2023-12-14 14:48:38 +0330

    made comment app

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post,Comment
 
-admin.site.register(Post)
+class CommentAdminInline(admin.TabularInline):
+    model=Comment
+    fields=['text',]
+    extra=0
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['id','title','is_enable','updated_time']
+    inlines=[CommentAdminInline,]
+
+#admin.site.register(Post,PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Post(models.Model):
     title=models.CharField(max_length=50)
-    text=models.TextField()
+    text=models.TextField(blank=True,null=True)
     is_enable=models.BooleanField(default=False)
-    created_time=models.DateTimeField()
-    updated_time=models.DateTimeField()
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    text=models.TextField()
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
