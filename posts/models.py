20aabff22868786bb839e85b43c736df002030d4
shahryar motamedi <shahryar.motamedi@yahoo.com>
2023-12-14 09:51:25 +0330

    made post moddel

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    text=models.TextField()
+    is_enable=models.BooleanField(default=False)
+    created_time=models.DateTimeField()
+    updated_time=models.DateTimeField()
